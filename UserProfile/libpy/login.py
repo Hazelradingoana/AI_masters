050c0704b1589fbc9535107d83f08b47ba95cb6f
Thembelihle Mncube <thembelihletaylor254@gmail.com>
2023-08-05 23:48:34 +0200

    use myApp.py to run the app

--- UserProfile/libpy/login.py
+++
@@ -1,9 +0,0 @@
-from kivy.uix.screenmanager import Screen
-from kivy.uix.button import Button
-
-
-class Login(Screen):
-    def __init__(self, **kwargs):
-        super(Login, self).__init__(**kwargs)
-        self.add_widget(Button(text='Go to Second Screen'))
-    pass
