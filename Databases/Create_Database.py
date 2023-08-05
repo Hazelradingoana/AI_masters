6a85b544e51fbaf6b9c82c3eb87977f01de1148e
haradin022 <haradin022@student.wethinkode.co.za>
2023-08-05 15:39:48 +0200

    Creating database for sign_up with ORM

--- Databases/Create_Database.py
+++ Databases/Create_Database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, Column, Integer, String, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
+
 
 Base = declarative_base()
 
@@ -8,6 +10,7 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     username = Column(String(50), unique=True)
+    user_email = Column(String(50), unique=True)
     password = Column(String(128))
 
 # Create the SQLite database
@@ -16,3 +19,14 @@ Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
+def create_user(username,user_email, password):
+    try:
+        user = User( password=password,username=username,user_email=user_email)
+        session.add(user)
+        session.commit()
+        print("User created successfully!")
+    except IntegrityError:
+        session.rollback()
+        print("Email already exists. Please choose a different Email.")
